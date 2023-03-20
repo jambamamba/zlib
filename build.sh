c315144b46758cffe50666c6254282eb667ff0e8
oosman <oosman@foo.bar>
2023-03-20 18:07:13 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,89 +1,16 @@
 #!/bin/bash -xe
 set -xe
 
-source toolchains/helper-functions.sh
-
-# function package(){
-#     local artifacts_url="/home/${USER}/Downloads"
-#     local target=""
-#     local library="zlib"
-# 	parseArgs $@
-# 	local workdir=installs
-# 	mkdir -p "${workdir}"
-#     rm -fr ${workdir}/*
-
-# 	mkdir -p "${workdir}/include"
-# 	rsync -uav *.h "${workdir}/include/"
-# 	mkdir -p "${workdir}/lib"
-#     if [ "${target}" == "mingw" ]; then
-#         rsync -uav ${target}-build/lib*.dll* "${workdir}/lib/"
-#     else
-#         rsync -uav ${target}-build/lib*.so* "${workdir}/lib/"
-#     fi
-
-#     pushd "${workdir}/lib/"
-#     stripArchive target="${target}"
-#     popd
-
-#     local SHA="$(sudo git config --global --add safe.directory .;sudo git rev-parse --verify --short HEAD)"
-#     local output="${library}-${SHA}-${target}.tar.xz"
-#     tar -cvJf "${output}" "${workdir}"
-
-#     if [ "${artifacts_url:0:8}" == "https://" ]; then
-#         echo "Not implemented! I quit."
-#         exit -1
-#     elif [ "${artifacts_url:0:1}" == "/" ] && [ -d "${artifacts_url}" ]; then
-#         cp -f "${output}" "${artifacts_url}/${artifacts_file}"
-#     fi
-#     mv -f "${output}" "${target}-build/"
-#     #rm -fr "${workdir}"
-# }
-
-# function sourceToolchainsFile(){
-#     local target="x86"
-#     local cmake_toolchain_file
-#     local bash_toolchain_file
-#     parseArgs $@
-
-#     if [ "$target" == "mingw" ]; then
-#         local workdir=$(mktemp -d)
-#         local curdir="$(pwd)"
-#         pushd "${workdir}"
-#         git clone --no-checkout --depth 1 git@github.com:jambamamba/utils.git
-#         cd utils
-#         local toolchainfile="x86_64-w64-mingw32.cmake"
-#         cmake_toolchain_file="${curdir}/${toolchainfile}"
-#         git show main:toolchains/${toolchainfile} > "${cmake_toolchain_file}"
-#         local toolchainfile="x86_64-w64-mingw32.sh"
-#         bash_toolchain_file="${curdir}/${toolchainfile}"
-#         git show main:toolchains/${toolchainfile} > "${bash_toolchain_file}"
-#         popd
-#         # rm -fr "${workdir}"
-#         if [ ! -f "${curdir}/${toolchainfile}" ]; then
-#             echo "Need full path to toolchain bash script that can be sourced"
-#             exit -1
-#         fi
-#         source "${bash_toolchain_file}"
-#     elif [ "$target" == "arm" ]; then
-#         local toolchainfile="${SDK_DIR}/environment-setup-aarch64-fslc-linux"
-#         if [ ! -f "${toolchainfile}" ]; then
-#             echo "Need full path to toolchain bash script that can be sourced"
-#             exit -1
-#         fi
-#         sudo mv -f ${SDK_DIR}/sysroots/x86_64-fslcsdk-linux/usr/bin/perl ${SDK_DIR}/sysroots/x86_64-fslcsdk-linux/usr/bin/perl.backup
-#         sudo ln -sf /usr/bin/perl ${SDK_DIR}/sysroots/x86_64-fslcsdk-linux/usr/bin/perl
-
-#         source ${toolchainfile}
-#         unset CROSS_COMPILE
-#         #sudo mv -f ${SDK_DIR}/sysroots/x86_64-fslcsdk-linux/usr/bin/perl.backup ${SDK_DIR}/sysroots/x86_64-fslcsdk-linux/usr/bin/perl
-#     else
-#         export STRIP=$(which strip)
-#     fi
-# }
+source share/scripts/helper-functions.sh
 
 function skip(){
     local target="x86"
     parseArgs $@
+
+    if [ "$clean" == "true" ]; then
+        return 0
+    fi
+
     local builddir="${target}-build"
     local SHA="$(sudo git config --global --add safe.directory .;sudo git rev-parse --verify --short HEAD)"
     local package="${library}-${SHA}-${target}.tar.xz"
@@ -124,7 +51,7 @@ function build(){
     pushd "${builddir}"
 
     if [ "$target" == "mingw" ]; then
-        source "../toolchains/x86_64-w64-mingw32.sh"
+        source "../share/toolchains/x86_64-w64-mingw32.sh"
         cmake \
             -DCMAKE_INSTALL_BINDIR=$(pwd) \
             -DCMAKE_INSTALL_LIBDIR=$(pwd) \
